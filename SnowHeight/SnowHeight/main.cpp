734fda57f3484a9bd3f5c3cb0bebda6091c6d191
gbirkner <g.birkner@telfs.com>
2018-02-19 15:38:17 +0100

    Initial Setup2

---
+++ SnowHeight/SnowHeight/main.cpp
@@ -0,0 +1,18 @@
+/*
+ * SnowHeight.cpp
+ *
+ * Created: 19.02.2018 15:37:52
+ * Author : gert
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
