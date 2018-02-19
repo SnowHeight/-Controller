dc11ad1d7aa102b1dea0b1b7b79b30ac17df8eef
gbirkner <g.birkner@telfs.com>
2018-02-19 15:35:45 +0100

    Project creation

---
+++ SnowHeight/SnowHeight/library.cpp
@@ -0,0 +1,16 @@
+/*
+ * SnowHeight.cpp
+ *
+ * Created: 19.02.2018 15:35:12
+ * Author : gert
+ */ 
+
+#include <avr/io.h>
+
+
+/* Replace with your library code */
+int myfunc(void)
+{
+	return 0;
+}
+
